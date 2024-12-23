2a1c44330c973acc09619634f1a4b3ab629fb756
tacheraSasi <tacherasasi@gmail.com>
2024-12-23 03:29:10 +0300

    more ellie code ...

--- main.go
+++ main.go
@@ -13,8 +13,10 @@ func main() {
 		switch args[1] {
 		case "start":
 			start(args)
-		case "stop":
+		case "stop": 
 			stop(args)
+		case "restart":
+			restart(args)
 		default:
 			fmt.Println("Hello Tach, what can I do for you today?")
 		}
@@ -55,4 +57,24 @@ func stop(args []string) {
 	} else {
 		fmt.Println("Please specify a service to stop: 'apache', 'mysql', or 'all'.")
 	}
+}
+
+func restart(args []string) {
+    if len(args) > 2 {
+        switch args[2] {
+        case "apache":
+            action.StopApache()
+            action.StartApache()
+        case "mysql":
+            action.StopMysql()
+            action.StartMysql()
+        case "all":
+            action.StopAll()
+            action.StartAll()
+        default:
+            fmt.Println("Service not recognized. Please choose 'apache', 'mysql', or 'all'.")
+        }
+    } else {
+        fmt.Println("Please specify a service to restart: 'apache', 'mysql', or 'all'.")
+    }
 }
