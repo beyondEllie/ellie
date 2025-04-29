7a4f1ee350e2e0e8026291c372cc21fd22c4ac3d
tacherasasi <tacherasasi@gmail.com>
2025-04-30 01:18:00 +0300

    devtools struct

--- action/devInit.go
+++ action/devInit.go
@@ -2,6 +2,13 @@ package actions
 
 import "github.com/tacheraSasi/ellie/styles"
 
+type DevTool struct {
+	Name     string
+	CheckCmd string   // e.g. "git --version"
+	Install  []string // OS specific install commands
+}
+
+
 func DevInit(){
 	styles.InfoStyle.Println("Initializing development environment...")
 	styles.InfoStyle.Println("Please wait while we set up your environment.")
