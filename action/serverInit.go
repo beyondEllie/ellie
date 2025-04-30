35c3f6d3341f9deda34e8aa02a8b4184322ce940
tacherasasi <tacherasasi@gmail.com>
2025-04-30 09:55:53 +0300

    more changes on the server init go

--- action/serverInit.go
+++ action/serverInit.go
@@ -13,21 +13,18 @@ func ServerInit() {
 	// 	return
 	// }
 
-	// Get the server name from the user
 	serverName, err := utils.GetInput("Enter the server name: ")
 	if err != nil {
 		fmt.Println("Error reading input:", err)
 		return
 	}
 
-	// Create the server directory
 	err = os.Mkdir(serverName, 0755)
 	if err != nil {
 		fmt.Println("Error creating server directory:", err)
 		return
 	}
 
-	// Initialize the server with default settings
 	err = os.WriteFile(serverName+"/config.json", []byte("{}"), 0644)
 	if err != nil {
 		fmt.Println("Error creating config file:", err)
