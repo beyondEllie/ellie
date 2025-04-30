82078fcda30b88dce65b8c1fc36dfe9f0b12abc0
tacherasasi <tacherasasi@gmail.com>
2025-04-30 09:45:31 +0300

    server init

---
+++ action/serverInit.go
@@ -0,0 +1,38 @@
+package actions
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/tacheraSasi/ellie/utils"
+)
+
+func ServerInit() {
+	// if utils.IsServerInitialized() {
+	// 	fmt.Println("Server is already initialized.")
+	// 	return
+	// }
+
+	// Get the server name from the user
+	serverName, err := utils.GetInput("Enter the server name: ")
+	if err != nil {
+		fmt.Println("Error reading input:", err)
+		return
+	}
+
+	// Create the server directory
+	err = os.Mkdir(serverName, 0755)
+	if err != nil {
+		fmt.Println("Error creating server directory:", err)
+		return
+	}
+
+	// Initialize the server with default settings
+	err = os.WriteFile(serverName+"/config.json", []byte("{}"), 0644)
+	if err != nil {
+		fmt.Println("Error creating config file:", err)
+		return
+	}
+
+	fmt.Printf("Server '%s' initialized successfully.\n", serverName)
+}
