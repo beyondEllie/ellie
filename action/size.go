bcf6a900dcf62c69f06a58c9a5c6e7d14fa510fc
tacherasasi <tacherasasi@gmail.com>
2025-06-05 17:40:46 +0300

    Implement Size function to display directory and file sizes based on OS; add handling for macOS and default case for unknown OS

--- action/size.go
+++ action/size.go
@@ -1,4 +1,21 @@
 package actions
 
+import (
+	"runtime"
+
+	"github.com/tacheraSasi/ellie/styles"
+)
+
 // Displays the size of directories and files
-func Size(){}
+func Size(){
+	switch runtime.GOOS {
+	case "darwin":
+		SizeMac()
+	default:
+		styles.ErrorStyle.Println("Unknown OS") //TODO: will updated this to a better message later
+	}
+}
+
+func SizeMac(){
+
+}
